2428997dbb3a85f32c911bfbd2207ca6796ae727
andreearthur <arthurmatilde@gmail.com>
2026-08-12 21:56:38 -0300

    ex-01 feito

---
+++ Aulas/12-08-26/Lab02/ex01/app/src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Entre com o dia: ");
+int d = sc.nextInt();
+
+System.out.print("Entre com o mês: ");
+String m = sc.next().toLowerCase();
+
+switch (mes) {
+    case "mar":
+estacao = (dia <= 20) ? "Outono" : "Inverno";
+
+}        
+        break;
+
+    default:
+        break;
+}
+}
+}
